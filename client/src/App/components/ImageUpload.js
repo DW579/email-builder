59330b235a83496feb2f70c3f32f0cdf8ae0b340
Dustin Wurtz <dustinwurtz@Dustins-MacBook-Pro.local>
2021-02-03 08:26:36 -0800

    upload and client side validation of zip folder

--- client/src/App/components/ImageUpload.js
+++ client/src/App/components/ImageUpload.js
@@ -5,7 +5,30 @@ class ImageUpload extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {};
+        this.handleChange = this.handleChange.bind(this);
+
+        this.state = {
+            label: "Upload image zip",
+            valid: false
+        };
+    }
+
+    handleChange(e) {
+        console.log(e.target.files);
+
+        if(e.target.files[0].type === "application/zip") {
+            this.setState({
+                label: e.target.files[0].name,
+                valid: true
+            });
+        }
+        else {
+            this.setState({
+                label: "Please upload only zip files",
+                valid: false
+            });
+        }
+
     }
 
     render() {
@@ -13,12 +36,15 @@ class ImageUpload extends Component {
             <Form>
                 <Form.Group>
                     <Form.File id="formcheck-api-custom" custom>
-                        <Form.File.Input isValid />
-                        <Form.File.Label data-browse="Button text">
-                            Upload image zip
+                        <Form.File.Input onChange={this.handleChange} isValid={this.state.valid} />
+                        <Form.File.Label data-browse="Upload">
+                            {this.state.label}
                         </Form.File.Label>
-                        <Form.Control.Feedback type="valid">
-                            Uploaded!
+                        <Form.Control.Feedback>
+                            Upload Successful!
+                        </Form.Control.Feedback>
+                        <Form.Control.Feedback type="invalid">
+                            Please upload only zip file format
                         </Form.Control.Feedback>
                     </Form.File>
                 </Form.Group>
